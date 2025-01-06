00a5a66628ccb0c73876ca340fb69fc5e9e780c8
dbijl <d.bijl-1@tudelft.nl>
2025-01-06 15:24:06 +0100

    update demo setup initialisation to compatible with current core-tools version

--- examples/demo_station/setup_config/init_station.py
+++ examples/demo_station/setup_config/init_station.py
@@ -4,30 +4,42 @@ import os
 import setup_config
 
 from setup_config.setup_hardware import setup_hardware
-from core_tools.GUI.keysight_videomaps.data_getter.scan_generator_Virtual import fake_digitizer
+from pulse_lib.tests.mock_m3202a import MockM3202A
+
 
 def init_station():
-
     # load config file with the instrument settings
-    station = qcodes.Station(config_file = os.path.dirname(setup_config.__file__) + '\\instruments.yaml')
+    instrument_config_path = os.path.join(
+            os.path.dirname(setup_config.__file__),
+            'instruments.yaml'
+    )
+    station = qcodes.Station(config_file=instrument_config_path)
 
     station.load_instrument('D5a1')
     station.load_instrument('D5a2')
     station.load_instrument('D5a3')
-#    station.load_instrument('D5a1', spi_rack=None)
-#    station.load_instrument('D5a2', spi_rack=None)
-#    station.load_instrument('D5a3', spi_rack=None)
+    # station.load_instrument('D5a1', spi_rack=None)
+    # station.load_instrument('D5a2', spi_rack=None)
+    # station.load_instrument('D5a3', spi_rack=None)
 
     hw = setup_hardware()
     station.add_component(hw)
     station.load_instrument(
-            'gates', hardware = hw,
-            dac_sources = [station.D5a1, station.D5a2, station.D5a3])
-
+        'gates',
+        hardware=hw,
+        dac_sources=[
+            station.D5a1,
+            station.D5a2,
+            station.D5a3
+        ]
+    )
 
     # load the digitizer
-#   station.load_instrument('digitizer_keys')
-    dig = fake_digitizer('digitizer_keys')
+    dig = MockM3202A(
+            name="Dig1",
+            chassis=1,
+            slot=11
+    )
     station.add_component(dig)
 
     station.load_instrument('AWG1')
