a28959115b604eb674289c1b5bd4fa0281568368
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2025-02-13 11:29:14 +0100

    Bump to 1.5.9

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="core_tools",
-    version="1.5.8",
+    version="1.5.9",
     packages = find_packages(),
     python_requires=">=3.10",
     install_requires=[
