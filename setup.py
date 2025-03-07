849c2442c832b631209911973c10882468f487e0
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-03-07 09:44:50 +0100

    Bump to 1.5.12

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name="core_tools",
-    version="1.5.11",
+    version="1.5.12",
     packages = find_packages(),
     python_requires=">=3.10",
     install_requires=[
