0c4010ea6b89193010969a99d79ce271bd6eb4de
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-04-25 15:31:31 +0200

    Added find_all_uuids_between

--- core_tools/data/sqdl/sqdl_reader.py
+++ core_tools/data/sqdl/sqdl_reader.py
@@ -1,12 +1,12 @@
-from functools import partial
 import io
 import os
 import shutil
 import requests
 import xarray as xr
-from datetime import datetime
+from datetime import datetime, timedelta
 from dataclasses import dataclass
 from concurrent.futures import ThreadPoolExecutor
+from functools import partial
 
 from tqdm import tqdm
 from sqdl_client.client import QDLClient
@@ -49,6 +49,19 @@ class DatasetReader:
     def list_scopes(self):
         sqdl_api = self.client.api
         return [scope.name for scope in sqdl_api.scope.list()]
+
+    def get_dataset_info(self, uuid) -> DatasetInfo:
+        ds_info = self.scope.retrieve_dataset_from_uid(uuid)
+        return DatasetInfo(
+            ds_info.uid,
+            ds_info.name,
+            ds_info.date_collected,
+            ds_info.metadata['setup'],
+            ds_info.metadata['sample'],
+            ds_info.rating > 0,
+            ds_info.metadata['variables_measured'],
+            ds_info.metadata['dimensions'],
+        )
 
     def load_ds_by_uuid(self, uuid) -> data_set:
         uid_str = str(int(uuid))
@@ -103,7 +116,7 @@ class DatasetReader:
             starred: bool = False,
             variables: list[str] | str | None = None,
             axes: list[str] | str | None = None,
-            ) -> list[DatasetInfo]:
+    ) -> list[DatasetInfo]:
         data_identifiers = {}
         if sample is not None:
             data_identifiers['sample'] = sample
@@ -115,13 +128,13 @@ class DatasetReader:
             data_identifiers['dimensions'] = axes
 
         sqdl_res = self.scope.search_datasets(
-            dataset_name_contains = name_contains,
-            collected_since = start_time,
-            collected_until = end_time,
-            rating = 1 if starred else None,
-            data_identifiers = data_identifiers,
+            dataset_name_contains=name_contains,
+            collected_since=start_time,
+            collected_until=end_time,
+            rating=(1 if starred else None),
+            data_identifiers=data_identifiers,
             limit=1000,
-            )
+        )
 
         res = []
         for r in sqdl_res:
@@ -135,8 +148,8 @@ class DatasetReader:
                     r.rating > 0,
                     r.metadata['variables_measured'],
                     r.metadata['dimensions'],
-                    )
                 )
+            )
 
         return res
 
@@ -165,26 +178,26 @@ def sqdl_logout():
 
 
 def sqdl_query(
-            name_contains: str = None,
-            start_time: datetime = None,
-            end_time: datetime = None,
-            setup: str = None,
-            sample: str = None,
-            starred: bool = False,
-            variables: list[str] | str | None = None,
-            axes: list[str] | str | None = None,
-            ) -> list[DatasetInfo]:
+    name_contains: str = None,
+    start_time: datetime = None,
+    end_time: datetime = None,
+    setup: str = None,
+    sample: str = None,
+    starred: bool = False,
+    variables: list[str] | str | None = None,
+    axes: list[str] | str | None = None,
+) -> list[DatasetInfo]:
 
     return _get_dataset_reader().query_datasets(
-            name_contains=name_contains,
-            start_time=start_time,
-            end_time=end_time,
-            setup=setup,
-            sample=sample,
-            starred=starred,
-            variables=variables,
-            axes=axes,
-        )
+        name_contains=name_contains,
+        start_time=start_time,
+        end_time=end_time,
+        setup=setup,
+        sample=sample,
+        starred=starred,
+        variables=variables,
+        axes=axes,
+    )
 
 
 def list_scopes():
@@ -227,4 +240,28 @@ def download_hdf5_parallel(
             executor.map(
                 partial(reader.download_hdf5_by_uid, download_dir=download_dir),
                 iterator)
-            )
+        )
+
+
+def find_all_uuids_between(start_uuid: int, end_uuid: int) -> list[int]:
+    """
+    Find all dataset UUIDs between a start and end UUID based on their start timestamps.
+
+    Args:
+        start_uuid (int): The UUID of the starting dataset.
+        end_uuid (int): The UUID of the ending dataset.
+
+    Returns:
+        list[int]: list of UUIDs from the start to the end timestamp.
+    """
+    ds_reader = _get_dataset_reader()
+    info_start = ds_reader.get_dataset_info(start_uuid)
+    info_end = ds_reader.get_dataset_info(end_uuid)
+
+    start_time = info_start.start_time
+    # add 1 microsecond, because end time is exclusive in query.
+    end_time = info_end.start_time + timedelta(microseconds=1)
+
+    datasets = sqdl_query(start_time=start_time, end_time=end_time)
+    dataset_uuids = [int(dataset.uuid) for dataset in sorted(datasets, key=lambda x: x.start_time)]
+    return dataset_uuids
