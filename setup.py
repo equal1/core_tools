773b8c9a750b4a9cad2f09bbb50fd95634419d9b
Sander de Snoo <59472150+sldesnoo-Delft@users.noreply.github.com>
2025-07-24 15:49:40 +0200

    Bump to 1.6.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="core_tools",
-    version="1.6.5",
+    version="1.6.6",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[
