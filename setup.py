efc84ced37925ec957bc3329648d26a9dfa959ef
dbijl <d.bijl-1@tudelft.nl>
2025-08-07 13:02:15 +0200

    Bump to 1.6.7

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="core_tools",
-    version="1.6.6",
+    version="1.6.7",
     packages=find_packages(),
     python_requires=">=3.10",
     install_requires=[
