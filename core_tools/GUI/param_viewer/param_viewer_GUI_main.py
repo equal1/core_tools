53d1443c9ced87bfb82f3d9b0a41545edb1b5ceb
sldesnoo-Delft <s.l.desnoo@tudelft.nl>
2025-10-09 12:45:07 +0200

    Fixed bug with step size of favourites.

--- core_tools/GUI/param_viewer/param_viewer_GUI_main.py
+++ core_tools/GUI/param_viewer/param_viewer_GUI_main.py
@@ -231,6 +231,7 @@ class param_viewer(QtWidgets.QMainWindow):
         voltage_input.setValue(parameter())
         voltage_input.valueChanged.connect(lambda: self._set_gate(parameter, voltage_input))
         voltage_input.setKeyboardTracking(False)
+        voltage_input.setSingleStep(self._step_size)
         layout.addWidget(voltage_input, row, 1, 1, 1)
 
         gate_unit = QtWidgets.QLabel()
